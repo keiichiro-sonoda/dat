7bd9b8ae3b6bbf45b6766ce108df2e0b03bc8581
keiichiro-sonoda <mountain13577749@gmail.com>
2021-02-03 10:04:58 +0900

    引数の表示ができた

--- hello01.c
+++ hello01.c
@@ -4,5 +4,8 @@ typedef char** char_pp;
 
 int main(int argv, char_pp argc) {
     printf("引数の数 %d 個\n", argv - 1);
+    for (int i = 1; i < argv; i++) {
+        printf("引数%02d: %s\n", i, argc[i]);
+    }
     return 0;
 }
